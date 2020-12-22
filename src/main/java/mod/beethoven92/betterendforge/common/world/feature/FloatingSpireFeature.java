4b6249de0735094615a9e6b7ded379552c21fa3d
Beethoven92 <65855508+Beethoven92@users.noreply.github.com>
2020-12-22 22:18:15 +0100

    floating spire

---
+++ src/main/java/mod/beethoven92/betterendforge/common/world/feature/FloatingSpireFeature.java
@@ -0,0 +1,72 @@
+package mod.beethoven92.betterendforge.common.world.feature;
+
+import java.util.List;
+import java.util.Random;
+
+import com.google.common.collect.Lists;
+
+import mod.beethoven92.betterendforge.common.init.ModBiomes;
+import mod.beethoven92.betterendforge.common.init.ModFeatures;
+import mod.beethoven92.betterendforge.common.util.ModMathHelper;
+import mod.beethoven92.betterendforge.common.util.sdf.SDF;
+import mod.beethoven92.betterendforge.common.util.sdf.operator.SDFDisplacement;
+import mod.beethoven92.betterendforge.common.util.sdf.primitive.SDFSphere;
+import mod.beethoven92.betterendforge.common.world.generator.OpenSimplexNoise;
+import net.minecraft.block.Blocks;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.ISeedReader;
+import net.minecraft.world.gen.ChunkGenerator;
+import net.minecraft.world.gen.Heightmap.Type;
+import net.minecraft.world.gen.feature.NoFeatureConfig;
+
+public class FloatingSpireFeature extends SpireFeature
+{
+	@Override
+	public boolean generate(ISeedReader world, ChunkGenerator generator, Random rand, BlockPos pos,
+		NoFeatureConfig config) 
+	{
+		int minY = world.getHeight(Type.WORLD_SURFACE, pos.getX(), pos.getZ());
+		int y = minY > 57 ? ModMathHelper.floor(ModMathHelper.randRange(minY, minY * 2, rand) * 0.5F + 32) : ModMathHelper.randRange(64, 192, rand);
+		pos = new BlockPos(pos.getX(), y, pos.getZ());
+		
+		SDF sdf = new SDFSphere().setRadius(ModMathHelper.randRange(2, 3, rand)).setBlock(Blocks.END_STONE);
+		int count = ModMathHelper.randRange(3, 5, rand);
+		
+		for (int i = 0; i < count; i++) 
+		{
+			float rMin = (i * 1.3F) + 2.5F;
+			sdf = addSegment(sdf, ModMathHelper.randRange(rMin, rMin + 1.5F, rand), rand);
+		}
+		for (int i = count - 1; i > 0; i--) 
+		{
+			float rMin = (i * 1.3F) + 2.5F;
+			sdf = addSegment(sdf, ModMathHelper.randRange(rMin, rMin + 1.5F, rand), rand);
+		}
+		
+		OpenSimplexNoise noise = new OpenSimplexNoise(rand.nextLong());
+		sdf = new SDFDisplacement().setFunction((vec) -> {
+			return (float) (Math.abs(noise.eval(vec.getX() * 0.1, vec.getY() * 0.1, vec.getZ() * 0.1)) * 3F + Math.abs(noise.eval(vec.getX() * 0.3, vec.getY() * 0.3 + 100, vec.getZ() * 0.3)) * 1.3F);
+		}).setSource(sdf);
+		final BlockPos center = pos;
+		List<BlockPos> support = Lists.newArrayList();
+		sdf.setReplaceFunction(REPLACE).setPostProcess((info) -> {
+			if (info.getStateUp().isAir()) {
+				if (rand.nextInt(16) == 0) {
+					support.add(info.getPos().up());
+				}
+				return world.getBiome(info.getPos()).getGenerationSettings().getSurfaceBuilderConfig().getTop();
+			}
+			return info.getState();
+		});
+		sdf.fillRecursive(world, center);
+		
+		support.forEach((bpos) -> {
+			if (ModBiomes.getFromBiome(world.getBiome(bpos)) == ModBiomes.BLOSSOMING_SPIRES) 
+			{
+				ModFeatures.TENANEA_BUSH.generate(world, generator, rand, bpos, null);
+			}
+		});
+		
+		return true;
+	}
+}
---
+++ src/main/java/mod/beethoven92/betterendforge/common/world/feature/SpireFeature.java
@@ -0,0 +1,110 @@
+package mod.beethoven92.betterendforge.common.world.feature;
+
+import java.util.List;
+import java.util.Random;
+import java.util.function.Function;
+
+import com.google.common.collect.Lists;
+import com.mojang.serialization.Codec;
+
+import mod.beethoven92.betterendforge.common.init.ModBiomes;
+import mod.beethoven92.betterendforge.common.init.ModFeatures;
+import mod.beethoven92.betterendforge.common.init.ModTags;
+import mod.beethoven92.betterendforge.common.util.ModMathHelper;
+import mod.beethoven92.betterendforge.common.util.sdf.SDF;
+import mod.beethoven92.betterendforge.common.util.sdf.operator.SDFDisplacement;
+import mod.beethoven92.betterendforge.common.util.sdf.operator.SDFSmoothUnion;
+import mod.beethoven92.betterendforge.common.util.sdf.operator.SDFTranslate;
+import mod.beethoven92.betterendforge.common.util.sdf.primitive.SDFSphere;
+import mod.beethoven92.betterendforge.common.world.generator.OpenSimplexNoise;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.LeavesBlock;
+import net.minecraft.block.material.Material;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.ISeedReader;
+import net.minecraft.world.gen.ChunkGenerator;
+import net.minecraft.world.gen.Heightmap.Type;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.NoFeatureConfig;
+
+public class SpireFeature extends Feature<NoFeatureConfig>
+{
+	protected static final Function<BlockState, Boolean> REPLACE;
+	
+	static {
+		REPLACE = (state) -> {
+			if (state.isIn(ModTags.END_GROUND)) 
+			{
+				return true;
+			}
+			if (state.getBlock() instanceof LeavesBlock) 
+			{
+				return true;
+			}
+			if (state.getMaterial().equals(Material.PLANTS)) 
+			{
+				return true;
+			}
+			return state.getMaterial().isReplaceable();
+		};
+	}
+	
+	public SpireFeature()
+	{
+		super(NoFeatureConfig.field_236558_a_);
+	}
+
+	@Override
+	public boolean generate(ISeedReader world, ChunkGenerator generator, Random rand, BlockPos pos,
+			NoFeatureConfig config)
+	{
+		pos = world.getHeight(Type.WORLD_SURFACE_WG, pos);
+		
+		if (pos.getY() > 57) 
+		{
+			SDF sdf = new SDFSphere().setRadius(ModMathHelper.randRange(2, 3, rand)).setBlock(Blocks.END_STONE);
+			int count = ModMathHelper.randRange(3, 7, rand);
+			for (int i = 0; i < count; i++) 
+			{
+				float rMin = (i * 1.3F) + 2.5F;
+				sdf = addSegment(sdf, ModMathHelper.randRange(rMin, rMin + 1.5F, rand), rand);
+			}
+			OpenSimplexNoise noise = new OpenSimplexNoise(rand.nextLong());
+			sdf = new SDFDisplacement().setFunction((vec) -> {
+				return (float) (Math.abs(noise.eval(vec.getX() * 0.1, vec.getY() * 0.1, vec.getZ() * 0.1)) * 3F + Math.abs(noise.eval(vec.getX() * 0.3, vec.getY() * 0.3 + 100, vec.getZ() * 0.3)) * 1.3F);
+			}).setSource(sdf);
+			final BlockPos center = pos;
+			List<BlockPos> support = Lists.newArrayList();
+			sdf.setReplaceFunction(REPLACE).setPostProcess((info) -> {
+				if (info.getStateUp().isAir())
+				{
+					if (rand.nextInt(16) == 0) 
+					{
+						support.add(info.getPos().up());
+					}
+					return world.getBiome(info.getPos()).getGenerationSettings().getSurfaceBuilderConfig().getTop();
+				}
+				return info.getState();
+			});
+			sdf.fillRecursive(world, center);
+			
+			support.forEach((bpos) -> {
+				if (ModBiomes.getFromBiome(world.getBiome(bpos)) == ModBiomes.BLOSSOMING_SPIRES) 
+				{
+					ModFeatures.TENANEA_BUSH.generate(world, generator, rand, bpos, null);
+				}
+			});
+			
+			return true;
+		}
+		return false;
+	}
+	
+	protected SDF addSegment(SDF sdf, float radius, Random random)
+	{
+		SDF sphere = new SDFSphere().setRadius(radius).setBlock(Blocks.END_STONE);
+		SDF offseted = new SDFTranslate().setTranslate(0, radius + random.nextFloat() * 0.25F * radius, 0).setSource(sdf);
+		return new SDFSmoothUnion().setRadius(radius * 0.5F).setSourceA(sphere).setSourceB(offseted);
+	}
+}
