6b14287534b9614dca9477220d30366d2c40b2ad
vemerion <vemerion@gmail.com>
2020-12-23 20:21:58 +0100

    Fix middle click mod entity to give spawn egg
    Since items are registered before entitytypes in Forge, this means that
    the mod spawn eggs will not be added to the EGGS map in the SpawnEggItem
    class, which in turn means that middle clicking entities in creative
    mode will not give the player the appropriate spawn egg. This was fixed by
    subscribing to the FMLCommonSetupEvent (where we know what all
    entitytypes have been registered), and use reflection to add all the mod spawn
    eggs to the EGGS map.

--- src/main/java/mod/beethoven92/betterendforge/common/item/ModSpawnEggItem.java
+++ src/main/java/mod/beethoven92/betterendforge/common/item/ModSpawnEggItem.java
@@ -1,7 +1,14 @@
 package mod.beethoven92.betterendforge.common.item;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.function.Supplier;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import mod.beethoven92.betterendforge.BetterEnd;
 import net.minecraft.block.DispenserBlock;
 import net.minecraft.dispenser.DefaultDispenseItemBehavior;
 import net.minecraft.dispenser.IBlockSource;
@@ -11,17 +18,28 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.item.SpawnEggItem;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.util.Direction;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 
+@EventBusSubscriber(modid = BetterEnd.MOD_ID, bus = Bus.MOD)
 public class ModSpawnEggItem extends SpawnEggItem {
-	
+
+	private static final Logger LOGGER = LogManager.getLogger();
+
+	private static final Map<Supplier<EntityType<?>>, SpawnEggItem> MOD_EGGS = new HashMap<>();
+
 	private static final DefaultDispenseItemBehavior SPAWN_EGG_BEHAVIOR = new DefaultDispenseItemBehavior() {
-        public ItemStack dispenseStack(IBlockSource source, ItemStack stack) {
-            Direction direction = source.getBlockState().get(DispenserBlock.FACING);
-            ((SpawnEggItem)stack.getItem()).getType(stack.getTag()).spawn(source.getWorld(), stack, null, source.getBlockPos().offset(direction), SpawnReason.DISPENSER, direction != Direction.UP, false);
-            stack.shrink(1);
-            return stack;
-        }
-    };
+		public ItemStack dispenseStack(IBlockSource source, ItemStack stack) {
+			Direction direction = source.getBlockState().get(DispenserBlock.FACING);
+			((SpawnEggItem) stack.getItem()).getType(stack.getTag()).spawn(source.getWorld(), stack, null,
+					source.getBlockPos().offset(direction), SpawnReason.DISPENSER, direction != Direction.UP, false);
+			stack.shrink(1);
+			return stack;
+		}
+	};
 
 	private Supplier<EntityType<?>> type;
 
@@ -29,7 +47,8 @@ public class ModSpawnEggItem extends SpawnEggItem {
 			Properties builder) {
 		super(null, primaryColorIn, secondaryColorIn, builder);
 		this.type = typeIn;
-        DispenserBlock.registerDispenseBehavior(this, SPAWN_EGG_BEHAVIOR);
+		DispenserBlock.registerDispenseBehavior(this, SPAWN_EGG_BEHAVIOR);
+		MOD_EGGS.put(typeIn, this);
 	}
 
 	@Override
@@ -41,5 +60,20 @@ public class ModSpawnEggItem extends SpawnEggItem {
 			}
 		}
 		return type.get();
+	}
+
+	// Use reflection to add the mod spawn eggs to the EGGS map in SpawnEggItem
+	@SubscribeEvent
+	public static void setup(FMLCommonSetupEvent event) {
+		event.enqueueWork(() -> {
+			try {
+			Map<EntityType<?>, SpawnEggItem> eggs = ObfuscationReflectionHelper.getPrivateValue(SpawnEggItem.class,
+					null, "field_195987_b");
+			for (Entry<Supplier<EntityType<?>>, SpawnEggItem> entry : MOD_EGGS.entrySet())
+				eggs.put(entry.getKey().get(), entry.getValue());
+			} catch (Exception e) {
+				LOGGER.warn("Unable to access SpawnEggItem.EGGS");
+			} 
+		});
 	}
 }
