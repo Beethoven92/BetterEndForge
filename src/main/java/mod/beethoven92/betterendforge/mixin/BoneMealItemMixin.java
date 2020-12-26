cb040ef16c8bd4bb2a67a5ab686d55d6cded235b
Beethoven92 <65855508+Beethoven92@users.noreply.github.com>
2020-12-26 19:02:14 +0100

    fix underwater seeds not growing with bone meal

--- src/main/java/mod/beethoven92/betterendforge/mixin/BoneMealItemMixin.java
+++ src/main/java/mod/beethoven92/betterendforge/mixin/BoneMealItemMixin.java
@@ -7,7 +7,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import mod.beethoven92.betterendforge.BetterEnd;
 import mod.beethoven92.betterendforge.common.init.ModBiomes;
 import mod.beethoven92.betterendforge.common.init.ModBlocks;
 import mod.beethoven92.betterendforge.common.init.ModTags;
