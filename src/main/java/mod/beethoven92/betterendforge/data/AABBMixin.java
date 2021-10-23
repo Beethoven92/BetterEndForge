81d57d1c8b00df1c5e9059bd63993eeea1358186
Someone Else Was Taken <justsomeoneelse91@gmail.com>
2021-10-23 00:57:16 -0700

    Created Access for ofSize

---
+++ src/main/java/mod/beethoven92/betterendforge/data/AABBMixin.java
@@ -0,0 +1,18 @@
+package mod.beethoven92.betterendforge.data;
+
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.vector.Vector3d;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.Inject;
+
+
+
+public class AABBMixin {
+
+
+
+    public static AxisAlignedBB ofSize(Vector3d vec3, double d, double e, double f) {
+        return new AxisAlignedBB(vec3.x - d / 2.0D, vec3.y - e / 2.0D, vec3.z - f / 2.0D, vec3.x + d / 2.0D, vec3.y + e / 2.0D, vec3.z + f / 2.0D);
+    }
+}
