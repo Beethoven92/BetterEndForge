e54fb89f2c024685445ff91ab4028f44cb0f3396
Someone Else Was Taken <justsomeoneelse91@gmail.com>
2021-10-22 19:41:14 -0700

    Added Umbral Valley Biome

---
+++ src/main/java/mod/beethoven92/betterendforge/common/world/biome/UmbraValleyBiome.java
@@ -0,0 +1,26 @@
+package mod.beethoven92.betterendforge.common.world.biome;
+
+import mod.beethoven92.betterendforge.common.init.*;
+import net.minecraft.world.gen.GenerationStage;
+//import ru.bclib.world.biomes.BCLBiomeDef;
+//import ru.betterend.BetterEnd;
+//import ru.betterend.registry.EndFeatures;
+//import ru.betterend.registry.EndParticles;
+//import ru.betterend.registry.EndSounds;
+//import ru.betterend.world.biome.EndBiome;
+//import ru.betterend.world.surface.SurfaceBuilders;
+
+public class UmbraValleyBiome extends BetterEndBiome
+{
+
+	public UmbraValleyBiome() {
+		super(new BiomeTemplate("umbra_valley")
+			.setFogColor(100, 100, 100)
+			.setFoliageColor(172, 189, 190)
+			.setWaterFogColor(69, 104, 134)
+			.setSurface(ModConfiguredSurfaceBuilders.get(ModConfiguredSurfaceBuilders.UMBRA_SURFACE))
+			.setParticles(ModParticleTypes.AMBER_SPHERE.get(), 0.0001F)
+			.setMusic(ModSoundEvents.AMBIENT_UMBRA_VALLEY.get())
+		);
+	}
+}
