636584249c2efe023f1465ae09b96c8269f944cb
Someone Else Was Taken <justsomeoneelse91@gmail.com>
2021-10-23 17:36:44 -0700

    Replaced End Spike with new Structure

--- src/main/java/mod/beethoven92/betterendforge/mixin/EndSpikeFeatureMixin.java
+++ src/main/java/mod/beethoven92/betterendforge/mixin/EndSpikeFeatureMixin.java
@@ -5,6 +5,7 @@ import java.util.Random;
 import mod.beethoven92.betterendforge.BetterEnd;
 import mod.beethoven92.betterendforge.common.util.BlockHelper;
 import mod.beethoven92.betterendforge.common.util.FeatureHelper;
+import mod.beethoven92.betterendforge.common.util.StructureHelper;
 import mod.beethoven92.betterendforge.common.util.WorldDataAPI;
 import mod.beethoven92.betterendforge.common.world.generator.GeneratorOptions;
 import net.minecraft.block.BlockState;
@@ -14,9 +15,12 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.item.EnderCrystalEntity;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.vector.Vector3i;
 import net.minecraft.world.IServerWorld;
 import net.minecraft.world.gen.Heightmap;
 import net.minecraft.world.gen.Heightmap.Type;
+import net.minecraft.world.gen.feature.template.PlacementSettings;
+import net.minecraft.world.gen.feature.template.Template;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -71,65 +75,110 @@ public abstract class EndSpikeFeatureMixin
 		GeneratorOptions.setDirectSpikeHeight();
 		int maxY = minY + spike.getHeight() - 64;
 
-		minY -= 15;
-		int r2 = radius * radius + 1;
-		BlockPos.Mutable mut = new BlockPos.Mutable();
-		for (int px = -radius; px <= radius; px++) {
-			mut.setX(x + px);
-			int x2 = px * px;
-			for (int pz = -radius; pz <= radius; pz++) {
-				mut.setZ(z + pz);
-				int z2 = pz * pz;
-				if (x2 + z2 <= r2) {
-					for (int py = minY; py < maxY; py++) {
-						mut.setY(py);
-						if (world.getBlockState(mut).getMaterial().isReplaceable()) {
-							BlockHelper.setWithoutUpdate(world, mut, Blocks.OBSIDIAN);
+		if (GeneratorOptions.replacePillars() && be_radiusInRange(radius)) {
+			radius--;
+			Template base = StructureHelper.readStructure(BetterEnd.makeID("pillars/pillar_base_" + radius));
+			Template top = StructureHelper.readStructure(BetterEnd.makeID("pillars/pillar_top_" + radius + (spike
+					.isGuarded() ? "_cage" : "")));
+			Vector3i side = base.getSize();
+			BlockPos pos1 = new BlockPos(x - (side.getX() >> 1), minY - 3, z - (side.getZ() >> 1));
+			minY = pos1.getY() + side.getY();
+			side = top.getSize();
+			BlockPos pos2 = new BlockPos(x - (side.getX() >> 1), maxY, z - (side.getZ() >> 1));
+			maxY = pos2.getY();
+
+			PlacementSettings data = new PlacementSettings();
+			base.func_237146_a_(world, pos1, pos1, data, random, 2);
+			top.func_237146_a_(world, pos2, pos2, data, random, 2);
+
+			int r2 = radius * radius + 1;
+			BlockPos.Mutable mut = new BlockPos.Mutable();
+			for (int px = -radius; px <= radius; px++) {
+				mut.setX(x + px);
+				int x2 = px * px;
+				for (int pz = -radius; pz <= radius; pz++) {
+					mut.setZ(z + pz);
+					int z2 = pz * pz;
+					if (x2 + z2 <= r2) {
+						for (int py = minY; py < maxY; py++) {
+							mut.setY(py);
+							if (world.getBlockState(mut).getMaterial().isReplaceable()) {
+								if ((px == radius || px == -radius || pz == radius || pz == -radius) && random.nextInt(
+										24) == 0) {
+									BlockHelper.setWithoutUpdate(world, mut, Blocks.CRYING_OBSIDIAN);
+								}
+								else {
+									BlockHelper.setWithoutUpdate(world, mut, Blocks.OBSIDIAN);
+								}
+							}
 						}
 					}
 				}
 			}
 		}
-		mut.setX(x);
-		mut.setZ(z);
-		mut.setY(maxY);
-		BlockHelper.setWithoutUpdate(world, mut, Blocks.BEDROCK);
+		else {
+			minY -= 15;
+			int r2 = radius * radius + 1;
+			BlockPos.Mutable mut = new BlockPos.Mutable();
+			for (int px = -radius; px <= radius; px++) {
+				mut.setX(x + px);
+				int x2 = px * px;
+				for (int pz = -radius; pz <= radius; pz++) {
+					mut.setZ(z + pz);
+					int z2 = pz * pz;
+					if (x2 + z2 <= r2) {
+						for (int py = minY; py < maxY; py++) {
+							mut.setY(py);
+							if (world.getBlockState(mut).getMaterial().isReplaceable()) {
+								BlockHelper.setWithoutUpdate(world, mut, Blocks.OBSIDIAN);
+							}
+						}
+					}
+				}
+			}
 
-		EnderCrystalEntity crystal = EntityType.END_CRYSTAL.create(world.getWorld());
-		crystal.setBeamTarget(config.getCrystalBeamTarget());
-		crystal.setInvulnerable(config.isCrystalInvulnerable());
-		crystal.setLocationAndAngles(x + 0.5D, maxY + 1, z + 0.5D, random.nextFloat() * 360.0F, 0.0F);
-		world.addEntity(crystal);
+			mut.setX(x);
+			mut.setZ(z);
+			mut.setY(maxY);
+			BlockHelper.setWithoutUpdate(world, mut, Blocks.BEDROCK);
 
-		if (spike.isGuarded()) {
-			for (int px = -2; px <= 2; ++px) {
-				boolean bl = MathHelper.abs(px) == 2;
-				for (int pz = -2; pz <= 2; ++pz) {
-					boolean bl2 = MathHelper.abs(pz) == 2;
-					for (int py = 0; py <= 3; ++py) {
-						boolean bl3 = py == 3;
-						if (bl || bl2 || bl3) {
-							boolean bl4 = px == -2 || px == 2 || bl3;
-							boolean bl5 = pz == -2 || pz == 2 || bl3;
-							BlockState blockState = (BlockState) ((BlockState) ((BlockState) ((BlockState) Blocks.IRON_BARS
-									.getDefaultState()
-									.with(PaneBlock.NORTH, bl4 && pz != -2)).with(
-									PaneBlock.SOUTH,
-									bl4 && pz != 2
-							)).with(PaneBlock.WEST, bl5 && px != -2)).with(
-									PaneBlock.EAST,
-									bl5 && px != 2
-							);
-							BlockHelper.setWithoutUpdate(
-									world,
-									mut.setPos(spike.getCenterX() + px, maxY + py, spike.getCenterZ() + pz),
-									blockState
-							);
+			EnderCrystalEntity crystal = EntityType.END_CRYSTAL.create(world.getWorld());
+			crystal.setBeamTarget(config.getCrystalBeamTarget());
+			crystal.setInvulnerable(config.isCrystalInvulnerable());
+			crystal.setLocationAndAngles(x + 0.5D, maxY + 1, z + 0.5D, random.nextFloat() * 360.0F, 0.0F);
+			world.addEntity(crystal);
+
+			if (spike.isGuarded()) {
+				for (int px = -2; px <= 2; ++px) {
+					boolean bl = MathHelper.abs(px) == 2;
+					for (int pz = -2; pz <= 2; ++pz) {
+						boolean bl2 = MathHelper.abs(pz) == 2;
+						for (int py = 0; py <= 3; ++py) {
+							boolean bl3 = py == 3;
+							if (bl || bl2 || bl3) {
+								boolean bl4 = px == -2 || px == 2 || bl3;
+								boolean bl5 = pz == -2 || pz == 2 || bl3;
+								BlockState blockState = (BlockState) ((BlockState) ((BlockState) ((BlockState) Blocks.IRON_BARS
+										.getDefaultState()
+										.with(PaneBlock.NORTH, bl4 && pz != -2)).with(
+										PaneBlock.SOUTH,
+										bl4 && pz != 2
+								)).with(PaneBlock.WEST, bl5 && px != -2)).with(
+										PaneBlock.EAST,
+										bl5 && px != 2
+								);
+								BlockHelper.setWithoutUpdate(
+										world,
+										mut.setPos(spike.getCenterX() + px, maxY + py, spike.getCenterZ() + pz),
+										blockState
+								);
+							}
 						}
 					}
 				}
 			}
 		}
+		info.cancel();
 	}
 
 	private boolean be_radiusInRange(int radius) {
