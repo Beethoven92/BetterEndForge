be727af1df3114e122335cf57cd3f07cf7c42233
Someone Else Was Taken <justsomeoneelse91@gmail.com>
2021-10-23 17:47:33 -0700

    Replaced End Podium

--- src/main/java/mod/beethoven92/betterendforge/mixin/EndPodiumFeatureMixin.java
+++ src/main/java/mod/beethoven92/betterendforge/mixin/EndPodiumFeatureMixin.java
@@ -4,11 +4,14 @@ import java.util.Random;
 
 import mod.beethoven92.betterendforge.BetterEnd;
 import mod.beethoven92.betterendforge.common.util.StructureHelper;
+import mod.beethoven92.betterendforge.common.util.WorldDataAPI;
 import mod.beethoven92.betterendforge.common.world.generator.GeneratorOptions;
+import net.minecraft.nbt.NBTUtil;
 import net.minecraft.util.math.vector.Vector3i;
 import net.minecraft.world.chunk.ChunkStatus;
 import net.minecraft.world.gen.feature.EndSpikeFeatureConfig;
 import net.minecraft.world.gen.Heightmap;
+import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.template.PlacementSettings;
 import net.minecraft.world.gen.feature.template.Template;
 import org.spongepowered.asm.mixin.Final;
@@ -29,13 +32,45 @@ import net.minecraft.world.gen.feature.NoFeatureConfig;
 @Mixin(EndPodiumFeature.class)
 public abstract class EndPodiumFeatureMixin
 {
+	@Shadow @Final private boolean activePortal;
+
 	@Inject(method = "generate", at = @At("HEAD"), cancellable = true)
-	private void beGeneratePortal(ISeedReader world, ChunkGenerator chunkGenerator, Random random, BlockPos blockPos,
+	private void beGeneratePortal(ISeedReader level, ChunkGenerator chunkGenerator, Random random, BlockPos origin,
 								  NoFeatureConfig config, CallbackInfoReturnable<Boolean> info)
 	{
 		if (!GeneratorOptions.hasPortal())
 		{
 			info.setReturnValue(false);
+			info.cancel();
 		}
+
+		else if (GeneratorOptions.replacePortal()) {
+			ISeedReader world = level;
+			BlockPos blockPos = be_updatePos(origin, world);
+			Template structure = StructureHelper.readStructure(BetterEnd.makeID(activePortal ? "portal/end_portal_active" : "portal/end_portal_inactive"));
+			Vector3i size = structure.getSize();
+			blockPos = blockPos.add(-(size.getX() >> 1), -1, -(size.getZ() >> 1));
+			structure.func_237146_a_(world, blockPos, blockPos, new PlacementSettings(), random, 2);
+			info.setReturnValue(true);
+			info.cancel();
+		}
+	}
+
+
+	private BlockPos be_updatePos(BlockPos blockPos, ISeedReader world) {
+		if (GeneratorOptions.useNewGenerator()) {
+			BlockPos pos = GeneratorOptions.getPortalPos();
+			if (pos.equals(BlockPos.ZERO)) {
+				int y = world.getChunk(0, 0, ChunkStatus.FULL).getTopBlockY(Heightmap.Type.WORLD_SURFACE, blockPos.getX(), blockPos.getZ());
+				if (y < 1) {
+					y = 65;
+				}
+				pos = new BlockPos(pos.getX(), y, pos.getZ());
+				GeneratorOptions.setPortalPos(pos);
+				WorldDataAPI.getRootTag(BetterEnd.MOD_ID).put("portal", NBTUtil.writeBlockPos(pos));
+			}
+			return pos;
+		}
+		return blockPos;
 	}
 }
